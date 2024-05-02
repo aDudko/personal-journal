f45a1d910f460e0004ea0eb7c4d7562a9b21b493
Anatoly Dudko <anatoly_dudko@icloud.com>
2024-05-02 21:12:03 +0200

    init.sql

--- init.sql
+++ init.sql
@@ -1,14 +1,1 @@
 CREATE DATABASE IF NOT EXIST personal-journal;
-
-CREATE TABLE IF NOT EXISTS post (
-    id          bigint default nextval('post_id_seq'::regclass) not null,
-    title       text,
-    create_date timestamp(6),
-    change_date text,
-    tag         text,
-    text        text,
-    status      text
-);
-
-ALTER TABLE IF EXISTS post
-    owner to postgres;
